8edac8e02e5ebc9858d34356c78e8b2d0bdfb5d5
David Gonzalez Martin <davidgm94.work@protonmail.com>
2021-05-25 18:25:53 +0200

    Fix build and run dependencies

--- build.zig
+++ build.zig
@@ -32,14 +32,6 @@ pub fn build(b: *std.build.Builder) void
     const run_step = b.addSystemCommand(cmd);
 
     const run_command = b.step("run", "Run the kernel");
+    run_command.dependOn(&exe.step);
     run_command.dependOn(&run_step.step);
-
-    //const run_cmd = exe.run();
-    //run_cmd.step.dependOn(b.getInstallStep());
-    //if (b.args) |args| {
-        //run_cmd.addArgs(args);
-    //}
-
-    //const run_step = b.step("run", "Run the app");
-    //run_step.dependOn(&run_cmd.step);
 }
